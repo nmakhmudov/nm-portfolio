296de4fc30909716080037349442470174fca2e5
nmakhmudov <nmakhmudow@gmail.com>
2023-03-13 13:16:46 +0300

    Preparing for deployment

--- pages/works/ludi.js
+++ pages/works/ludi.js
@@ -1,17 +1,17 @@
 import {
-  Box,
+//  Box,
   Container,
   Badge,
-  Link,
-  List,
-  ListItem,
-  SimpleGrid,
-  UnorderedList,
-  Image
+//  Link,
+//  List,
+//  ListItem,
+//  SimpleGrid,
+//  UnorderedList,
+//  Image
 } from '@chakra-ui/react'
 import Layout from '../../components/layouts/article'
-import { ExternalLinkIcon } from '@chakra-ui/icons'
-import { Title, WorkImage, Meta } from '../../components/work'
+//import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Title, /*WorkImage Meta*/} from '../../components/work'
 
 const Work = () => (
   <Layout title="Ludi">
