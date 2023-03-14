95b0554321b86ad0d3b6507df4a77594864e8e30
nmakhmudov <nmakhmudow@gmail.com>
2023-03-14 14:57:52 +0300

    Personal img added

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,7 @@ const Home = () => (
             overflow="hidden"
           >
             <ProfileImage
-              //src="/images/takuya.jpg"
+              src="/images/takuya.jpg"
               alt="Profile image"
               borderRadius="full"
               width="100"
