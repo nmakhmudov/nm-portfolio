008bc1934b682d039f8ad8333af7af928e3ca9ce
nmakhmudov <nmakhmudow@gmail.com>
2023-03-14 14:33:39 +0300

    Removed 3d elements

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,14 +1,9 @@
 import Head from 'next/head'
-import dynamic from 'next/dynamic'
 import NavBar from '../navbar'
 import { Box, Container } from '@chakra-ui/react'
 import Footer from '../footer'
-import VoxelDogLoader from '../voxel-dog-loader'
 
-const LazyVoxelDog = dynamic(() => import('../voxel-dog'), {
-  ssr: false,
-  loading: () => <VoxelDogLoader />
-})
+
 
 const Main = ({ children, router }) => {
   return (
@@ -35,7 +30,7 @@ const Main = ({ children, router }) => {
       <NavBar path={router.asPath} />
 
       <Container maxW="container.md" pt={14}>
-        <LazyVoxelDog />
+
 
         {children}
 
