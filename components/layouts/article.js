8a79e11eb64bd34b23635ff557adf86be9c2cab2
nmakhmudov <nmakhmudow@gmail.com>
2023-03-17 12:24:02 +0300

    Edited header

--- components/layouts/article.js
+++ components/layouts/article.js
@@ -9,7 +9,7 @@ const variants = {
 }
 
 const Layout = ({ children, title }) => {
-  const t = `${title} - Takuya Matsuyama`
+  const t = `${title} - Nijat Mcmudow`
   return (
     <motion.article
       initial="hidden"
