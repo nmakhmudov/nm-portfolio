6b38a407930509c9fca49925906799d0332764f5
nmakhmudov <nmakhmudow@gmail.com>
2023-05-04 15:41:01 +0300

    Reverse

--- pages/works/ludi.js
+++ pages/works/ludi.js
@@ -3,63 +3,27 @@ import {
   Container,
   Badge,
 //  Link,
-  List,
+//  List,
 //  ListItem,
   SimpleGrid,
 //  UnorderedList,
 //  Image
 } from '@chakra-ui/react'
 import Layout from '../../components/layouts/article'
-import { Title, WorkImage, Meta } from '../../components/work'
 //import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { Title, WorkImage } from '../../components/work'
 
 const Work = () => (
   <Layout title="Ludi">
     <Container>
-    <Title>
-        Epital <Badge>2011-2016</Badge>
+      <Title>
+         <Badge>2022</Badge>
       </Title>
       <div>
-        A Danish Telemedicine Platform for people with chronic obstructive pulmonary disease (COPD). 
-
+        Ludi stays for - Lung Disease classification. Based on the given unstructures image data, such as image of a chest X-ray, LuDi
+        can train its neural network to diagnose whether or not a patient suffers from one of the fourteen pathological conditions.
+        Algorithm could classify clinically important abnormalities on chest radiographs at a performance level comparable to practicing radiologists.
       </div>
-      <List ml={4} my={4}>
-        <ListItem>
-          <Meta>Website</Meta>
-          <Link href="https://epital.com/da/">
-            https://www.epital.com/ <ExternalLinkIcon mx="2px" />
-          </Link>
-        </ListItem>
-        <ListItem>
-          <Meta>Platform</Meta>
-          <span>Windows/macOS/Linux/iOS/Android</span>
-        </ListItem>
-        <ListItem>
-          <Meta>Language</Meta>
-          <span>Java, Python, Linux</span>
-        </ListItem>
-        <ListItem>
-          <Meta>Framework</Meta>
-          <span>Tornado, Flask, Ember, JQuery, Angular, D3</span>
-        </ListItem>
-        <ListItem>
-          <Meta>Web Platform</Meta>
-          <span>AWS, Google App Engine</span>
-        </ListItem>
-        <ListItem>
-          <Meta>Database/Caching</Meta>
-          <span>MongoDB, Redis, Nginx</span>
-        </ListItem>
-        <ListItem>
-          <Meta>Server Management</Meta>
-          <span>Docker</span>
-        </ListItem>
-        <ListItem>
-          <Meta>Protocols</Meta>
-          <span>TCP, FreeTDS</span>
-        </ListItem>
-      </List>
 
       <SimpleGrid columns={2} gap={2} >
         <WorkImage src="/images/works/x-ray.png" alt="ludi" />
